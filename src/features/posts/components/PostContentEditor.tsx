e07521bfc4e141db58d7d3c8868ff0292f2575a9
NguyenMinhNhat843 <NhatNguyen15062019@gmail.com>
2026-03-30 10:36:47 +0700

    fix: fix lỗi deploy vercel, tạm thời hidden tiptap editor

--- src/features/posts/components/PostContentEditor.tsx
+++ src/features/posts/components/PostContentEditor.tsx
@@ -1,8 +1,5 @@
 import { usePostForm } from "../hooks/usePostForm";
 import ToolbarButton from "./ToolbarButton";
-import { EditorContent, useEditor } from "@tiptap/react";
-import { FloatingMenu, BubbleMenu } from "@tiptap/react/menus";
-import StarterKit from "@tiptap/starter-kit";
 
 const PostContentEditor = () => {
   const {
@@ -14,20 +11,9 @@ const PostContentEditor = () => {
     wordCount,
     errors,
   } = usePostForm();
-  const editor = useEditor({
-    extensions: [StarterKit], // define your extension array
-    content: "<p>Hello World!</p>", // initial content
-  });
 
   return (
     <div>
-      <div>
-        TipTap
-        <EditorContent editor={editor} />
-        <FloatingMenu editor={editor}>This is the floating menu</FloatingMenu>
-        <BubbleMenu editor={editor}>This is the bubble menu</BubbleMenu>
-        TipTap
-      </div>
       <div className="bg-white rounded-2xl shadow-sm border border-slate-100 overflow-hidden">
         <div className="border-b border-slate-100 px-4 py-2 flex items-center gap-1 flex-wrap">
           <ToolbarButton
